a17e8dca17cbc79a8102ed9f551caba9d05c74ef
Omalya19 <wathsadiomalya19@gmail.com>
2026-07-17 21:23:10 +0530

    Fixed mobile layout & update script

--- JS/script.js
+++ JS/script.js
@@ -1,2 +1,17 @@
-// වෙබ් අඩවියට එන අයට Welcome මැසේජ් එකක් දාන්න උදාහරණයක්
+// Welcome massage
 console.log("Website successfully loaded!");
+
+// Contact Form
+function validateForm() {
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let message = document.getElementById("message").value;
+    
+    if (name === "" || email === "" || message === "") {
+        alert("Please fill in all fields.");
+        return false;
+    }
+
+    alert("Thank you! Your message has been sent.");
+    return true;
+}
