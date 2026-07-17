1510efa1c910d8e08dd2d9da955080092455fca9
Omalya19 <wathsadiomalya19@gmail.com>
2026-07-17 22:24:43 +0530

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,17 +1,4 @@
 // Welcome massage
 console.log("Website successfully loaded!");
 
-// Contact Form
-function validateForm() {
-    let name = document.getElementById("name").value;
-    let email = document.getElementById("email").value;
-    let message = document.getElementById("message").value;
-    
-    if (name === "" || email === "" || message === "") {
-        alert("Please fill in all fields.");
-        return false;
-    }
 
-    alert("Thank you! Your message has been sent.");
-    return true;
-}
